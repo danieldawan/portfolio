a8f1526bf27ae0087216a188f4ce317e15ac828a
Daniel Dawan <141849615+danieldawan@users.noreply.github.com>
2024-01-30 15:09:20 +0100

    Content updates

--- data/config.js
+++ data/config.js
@@ -1,9 +1,9 @@
 export const meta = {
   title: 'Daniel Dawan'
-}
+};
 
 export const hero = {
-  title: "Daniel Dawan",
+  title: 'Daniel Dawan'
 };
 
 export const projects = {
@@ -11,12 +11,20 @@ export const projects = {
   projects: [
     {
       title: 'AI Generative MIDI Sequencer',
-      description: 'Developed an application that generates MIDI sequences enabling automated music composition and enhancing production techniques by harnessing the power of coding and artificial intelligence.',
+      description:
+        'Developing an application that generates MIDI sequences, enabling automated music composition and enhancing production techniques by harnessing the power of coding and artificial intelligence. Expected completion in April 2024.',
       github: 'danieldawan/ai-midi-sequencer'
     },
     {
+      title: 'Flight Product Demand Forecasting',
+      description:
+        'This datathon challenges participants to create a model that predicts the number of products sold on each Ryanair flight, 10 days before take-off. The model will aim to aid the airline in adjusting provisions, optimizing inventory, and preventing overstocking. Expected completion in March 2024.',
+      github: 'danieldawan/sustainability-datathon-2024'
+    },
+    {
       title: 'NLP Text Analysis and Synthesis',
-      description: 'Developed an NLP toolkit for text processing, featuring unigram and bigram models for statistical analysis and synthesis, incorporating corpus preprocessing, frequency-based analysis, and text generation.',
+      description:
+        'Developed an NLP toolkit for text processing, featuring unigram and bigram models for statistical analysis and synthesis, incorporating corpus preprocessing, frequency-based analysis, and text generation. The toolkit also streamlines the processing of large text datasets, simplifying the development of LLMs.',
       github: 'danieldawan/nlp-project'
     }
   ]
@@ -27,13 +35,15 @@ export const stack = {
   stack: [
     'Python',
     'JavaScript',
-    'Git',
     'SQL',
     'HTML',
     'CSS',
     'PyTorch',
     'NumPy',
-    'Pandas'
+    'Pandas',
+    'Git',
+    'AWS',
+    'GCP'
   ]
 };
 
