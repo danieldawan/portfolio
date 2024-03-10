236b7b9fa1b4b5e50742ba21cf8fe58f5de7c4eb
Daniel Dawan <141849615+danieldawan@users.noreply.github.com>
2024-03-10 19:42:51 +0100

    Projects update

--- data/config.js
+++ data/config.js
@@ -15,11 +15,6 @@ export const projects = {
         'Developing an application that generates MIDI sequences, enabling automated music composition and enhancing production techniques by harnessing the power of coding and artificial intelligence. Expected completion in April 2024.'
     },
     {
-      title: 'Flight Product Demand Forecasting',
-      description:
-        'This datathon, in collaboration with Ryanair, challenges participants to create a model that predicts the number of products sold on each flight, 10 days before take-off. The model will aim to aid the airline in adjusting provisions, optimising inventory, and preventing overstocking.'
-    },
-    {
       title: 'Stock Portfolio Tracker',
       description:
         "Designed an online portfolio stock tracker using JavaScript and React components for the front-end, and Python for the back-end. The application enables accurate tracking of your investments by utilizing Alpha Vantage's stock data and leverages the OpenAI API for instant portfolio analyses. Published in March 2024."
